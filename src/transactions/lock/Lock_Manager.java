ac0e38221296832bc473d6806ec7f87b03993177
gabrieloureiro <39869298+gabrieloureiro@users.noreply.github.com>
2019-05-26 12:30:16 -0300

    Update Lock_Manager.java
    U

--- src/transactions/lock/Lock_Manager.java
+++ src/transactions/lock/Lock_Manager.java
@@ -10,6 +10,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.w3c.dom.ls.LSException;
+
 import transactions.Tr_Manager;
 
 /**
@@ -20,7 +22,7 @@ public class Lock_Manager {
     
     // columns: 0 - dado, 1 - id transaction, 2 - lock type
     Map<String, List<String>> Lock_Table = new HashMap<>();
-    
+    Wait_Q waitq = new Wait_Q();
     // columns: 0 - dado, 1 - queue of who's waiting for access the dado
     Map<String, Wait_Q> dado_queue = new HashMap<String, Wait_Q>();
     String shared, exclusive;
@@ -46,20 +48,19 @@ public class Lock_Manager {
         // caso contrario cria/atualiza a Wait_Q do dado em questao com a transacao.
         
         List<String> ls = new ArrayList<>(); //id_transaction and lock_type
-        Wait_Q w = new Wait_Q(); //id_transaction and lock_type
         ls.add(idTransaction);
         ls.add(isShared());
                                             //falta associar com timestamp
         if(dado_queue.isEmpty()==false){
             if(dado_queue.containsValue(dado)){
                 System.out.println("A fila já contém o dado.");
-                w = dado_queue.get(dado);
-                w.addToQueue(idTransaction, isShared());
+                waitq = dado_queue.get(dado);
+                waitq.addToQueue(idTransaction, isShared());
             }
 
             if(dado_queue.containsValue(dado)==false && Lock_Table.isEmpty()==false){
-                w.addToQueue(idTransaction, isShared());
-                dado_queue.put(dado, w);
+                waitq.addToQueue(idTransaction, isShared());
+                dado_queue.put(dado, waitq);
             }
 
         } else {
@@ -85,9 +86,15 @@ public class Lock_Manager {
 
     void U(String idTransaction, String dado){
         // Apaga o bloqueio da transacao sobre o item dado na Lock Table.
+        if (Lock_Table.containsValue(dado)){
+            Lock_Table.remove(idTransaction);
+        }
+        if (waitq.containsValue(idTransaction)){
+            waitq.removeFromQueue(idTransaction);
+        }
     }
     
-    void WaitDie(String idTransaction1, String idTransaction2){
+    void WaitDie(String idTransaction1, String idTransaction2, String dado){
         // Chame o metodo getTimestamp() do Tr_Manager para
         // pegar o timestamp das duas transacoes e decidir o que fazer
         // Seja Ti a transacao já com locked, e Tj a que acabou de chegar,
@@ -97,6 +104,14 @@ public class Lock_Manager {
         //
         // * Essa decisao vale tanto para Exclusive sobre Shared 
         // como para Shared sobre Exclusive
+        int time_one = getTimestamp(idTransaction1);
+        int time_two = getTimestamp(idTransaction2);
+
+        // if(){
+        //     if(time_one<time_two){
+
+        //     }
+    
     }
 
 
